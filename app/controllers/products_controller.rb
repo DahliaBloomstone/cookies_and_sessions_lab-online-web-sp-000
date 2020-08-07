721c637bdf73e985b782e4e9d8234d705762e141
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-08-07 20:42:24 +0000

    Automatically backed up by Learn

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,2 @@
+#Create a Products controller with two actions, index and add.
+#Create the routes for the application, we only need two routes, one to display the Products#index (the root route) and one to post the products to add them to the cart.
