4f1e61b217cac4a307a1669058451db4af28f7c7
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-08-07 21:12:29 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,23 @@ class ApplicationController < ActionController::Base
 
 
 #Create the cart method in ApplicationController#cart, this method should return an array of the items stored in the cart (utilizing the Rails session method).
-helper_method :cart 
+helper_method :cart
 
 
 def cart
   session[:cart] ||= []
   end
+end
+
+#same code up there wrapped up in a helper method 
+#def add_to_cart
+  # Get the item from the path
+#  @item = Item.find(params[:id])
+
+  # Load the cart from the session, or create a new empty cart.
+  #cart = session[:cart] || []
+  #cart << @item.id
+
+  # Save the cart in the session.
+  #session[:cart] = cart
 end
