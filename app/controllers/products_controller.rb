2469c82f7818679168fcd19423b5c1a862d90961
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-08-07 21:11:42 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,6 +6,7 @@ class ProductsController < ApplicationController
   end
 
   def add
+     # Load the cart from the session, or create a new empty cart.
     cart << params[:product]
     redirect_to '/'
   end
