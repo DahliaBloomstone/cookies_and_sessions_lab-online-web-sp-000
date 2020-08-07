7b56bc312ef15143e33445aaba7d717d720b07d6
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-08-07 20:44:39 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,11 @@
 #Create a Products controller with two actions, index and add.
 #Create the routes for the application, we only need two routes, one to display the Products#index (the root route) and one to post the products to add them to the cart.
+
+def add_to_cart
+  @product = Product.find(params[:id])
+
+  cart = session[:cart] || []
+  cart << @product.id
+
+  session[:cart] = cart
+end 
